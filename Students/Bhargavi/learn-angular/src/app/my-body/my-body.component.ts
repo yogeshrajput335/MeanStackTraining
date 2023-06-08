95af5120fa551e3a06cbe7c5291caf578f60edf5
Bhargavi Vemula <vemulabhargavi77@gmail.com>
2023-06-08 17:18:47 -0500

    correcting errors

--- Students/Bhargavi/learn-angular/src/app/my-body/my-body.component.ts
+++ Students/Bhargavi/learn-angular/src/app/my-body/my-body.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-body',
+    templateUrl: './my-body.component.html',
+    styleUrls: ['./my-body.component.css']
 })
 export class MyBodyComponent {
 
