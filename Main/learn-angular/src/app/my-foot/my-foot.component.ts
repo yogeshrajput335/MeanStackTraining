12cd8c719e7c8e77775fb1f1fe82cce01195ace4
Yogesh Rajput <yogeshrajput335@gmail.com>
2023-06-08 07:45:17 +0530

    added components

---
+++ Main/learn-angular/src/app/my-foot/my-foot.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-foot',
+  templateUrl: './my-foot.component.html',
+  styleUrls: ['./my-foot.component.css']
+})
+export class MyFootComponent {
+
+}
